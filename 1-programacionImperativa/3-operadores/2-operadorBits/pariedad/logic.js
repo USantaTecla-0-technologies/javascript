91d533b9b1cc146f12e8e2002b43ed7f473ef4bd
Setillo <setillofm@gmail.com>
2021-03-10 18:57:59 +0100

    refactor

--- 1-programacionImperativa/3-operadores/2-operadorBits/pariedad/logic.js
+++ 1-programacionImperativa/3-operadores/2-operadorBits/pariedad/logic.js
@@ -1,3 +1,4 @@
 let number = +prompt("Escribe un número? ");
-let parity = (number & 1 == 0) ? "par" : "impar";
+let parity = (number & 1 != 1) ? "par" : "impar";
 console.log("El numero " + number + " es " + parity);
+
