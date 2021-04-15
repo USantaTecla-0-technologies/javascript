f8f7f2e5b8babf8aa5513ac8337c68e864eef235
Setillo <setillofm@gmail.com>
2021-04-15 13:49:03 +0200

    refactor

--- 2-programacionEstructurada/4-tablas/0-tablas.gestion/logic.js
+++ 2-programacionEstructurada/4-tablas/0-tablas.gestion/logic.js
@@ -1,51 +1,92 @@
+/* creación */
 let array = [];
+console.log(typeof array);
+console.log(array);
+for(let i = 0; i < array.length; i++){
+    console.log(array[i]);
+}
+for(let item of array){
+    console.log(item);
+}
+for(let key in array){
+    console.log(array[key]);
+}
+console.log(array.length);
 
-/* añadir elementos al array asociativo */
+/* añadir elementos al array */
 array[0] = 10;
-console.log("length: " + array.length);
 for(let item of array){
     console.log(item);
 }
+for(let key in array){
+    console.log(array[key]);
+}
+console.log("length: " + array.length);
+
 array[1] = 11;
-console.log("length: " + array.length);
 for(let item of array){
     console.log(item);
 }
+for(let key in array){
+    console.log(array[key]);
+}
+console.log("length: " + array.length);
+
 array[99] = 990;
-console.log("length: " + array.length);
 for(let item of array){
     console.log(item);
 }
-array[-1] = -10;
+for(let key in array){
+    console.log(array[key]);
+}
 console.log("length: " + array.length);
+
+array[-1] = -10;
 for(let item of array){
     console.log(item);
 }
 console.log(array[-1]); 
+for(let key in array){
+    console.log(array[key]);
+}
+console.log("length: " + array.length);
 
 array["zzz"] = "zzz";
-console.log("length: " + array.length);
 for(let item of array){
     console.log(item);
 }
 console.log(array[-1]);
 console.log(array["zzz"]);
-
-/* modificar elementos del array asociativo */
-array[1] = 1111111;
+for(let key in array){
+    console.log(array[key]);
+}
 console.log("length: " + array.length);
+
+/* modificar elementos del array */
+array[1] = 1111111;
 for(let item of array){
     console.log(item);
 }
+for(let key in array){
+    console.log(array[key]);
+}
+console.log("length: " + array.length);
 
 /* eliminar elementos del array asociativo */
 delete array[1];
-console.log("length: " + array.length);
 for(let item of array){
     console.log(item);
 }
+for(let key in array){
+    console.log(array[key]);
+}
+console.log("length: " + array.length);
+
 delete array[99];
-console.log("length: " + array.length);
 for(let item of array){
     console.log(item);
 }
+for(let key in array){
+    console.log(array[key]);
+}
+console.log("length: " + array.length);
---
+++ draft/tabla como objetos/logic.js
@@ -0,0 +1,6 @@
+
+let x = [];
+x[3.3] = 2;
+console.log(x[3.3]);
+
+console.log(x instanceof Array);
